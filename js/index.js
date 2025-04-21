faa2ec914af5b5237a2d70424d79da8d356c1008
Yurii <yurilohan1000@gmail.com>
2025-04-20 22:56:04 -0300

    removendo o segundo js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
-function mostrarConteudo(numero) {
-    const conteudo = document.getElementById(`${numero}bim`);
-    const sections = document.querySelectorAll('section[id$="bim"]');
-    if(conteudo.style.display === 'none' || conteudo.style.display === '') {
-        sections.forEach(section => {
-            section.style.display = 'none';
-        });
-        conteudo.style.display = 'block';
-    }
-    else {
-        conteudo.style.display = 'none';
+function aparecerConteudo(numero){
+    const content = document.getElementById(`${numero}bim`)
+    const sections = document.querySelectorAll('section[id$="bim"]')
+            
+    if (content.style.display === 'none'|| content.style.display === '') {
+        
+        sections.forEach(section =>{
+            section.style.display = 'none'
+        })
+
+        content.style.display = 'block'
+    } else {
+        content.style.display = 'none'
     }
 }
